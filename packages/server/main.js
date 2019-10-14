0a1f7d18a90672d0879371108ca74ddcbbcac501
snacky <cliche90@naver.com>
2019-10-15 00:17:25 +0900

    add index router to webserver

--- packages/server/main.js
+++ packages/server/main.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+app.set('vue engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('/', require('./src/router/index'));
 app.use('/songs', require('./src/router/songs'));
 
 app.listen(process.env.SERVICE_PORT, () => console.log(`listen on ${process.env.SERVICE_PORT} port`));
--- packages/server/scripts/build-docker-image.sh
+++ packages/server/scripts/build-docker-image.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 SCHEMA_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd ../../crawler/schema ; pwd -P )
+FRONTEND_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd ../../frontend/dist ; pwd -P )
 WEBSERVER_ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd ../ ; pwd -P )
 
 rm -rf "$WEBSERVER_ROOT_DIR/schema"
 cp -r $SCHEMA_DIR $WEBSERVER_ROOT_DIR
+
+rm -rf "$WEBSERVER_ROOT_DIR/dist"
+cp -r $FRONTEND_DIR $WEBSERVER_ROOT_DIR
 
 cd $WEBSERVER_ROOT_DIR
 docker build -t web-music-player .
